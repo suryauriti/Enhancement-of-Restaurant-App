f6b52c6810b8f597e84c8723dcc5f792134b6d17
suryauriti <suryauriti8@gmail.com>
2024-02-09 09:42:38 +0000

    solution

---
+++ src/components/Header/index.js
@@ -0,0 +1,42 @@
+import {AiOutlineShoppingCart} from 'react-icons/ai'
+import './index.css'
+import Cookies from 'js-cookie'
+import {Link, withRouter} from 'react-router-dom'
+import CartContext from '../../context/CartContext'
+
+const Header = props => {
+  const {restaurantName} = props
+  return (
+    <CartContext.Consumer>
+      {value => {
+        const {cartList} = value
+
+        const onClickLogout = () => {
+          Cookies.remove('jwt_token')
+          const {history} = props
+          history.replace('/login')
+        }
+
+        return (
+          <div className="header-container">
+            <Link to="/" className="heading-link">
+              <h1>{restaurantName}</h1>
+            </Link>
+
+            <div className="icons-container">
+              <p className="my-orders">My Orders</p>
+              <Link to="/cart">{<AiOutlineShoppingCart size={40} />}</Link>
+              <p className="cart_count">{cartList.length}</p>
+
+              <button className="logout" type="button" onClick={onClickLogout}>
+                Logout
+              </button>
+            </div>
+          </div>
+        )
+      }}
+    </CartContext.Consumer>
+  )
+}
+
+export default withRouter(Header)
